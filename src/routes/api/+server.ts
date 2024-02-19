bf6df8e30fb7a7ac5d08bb0b01c2b1b02540ef91
Simre1 <simre4775@gmail.com>
2024-02-19 22:56:51 +0100

    Add initial backend converter

--- src/routes/api/+server.ts
+++ src/routes/api/+server.ts
@@ -1,9 +1,21 @@
 import type { RequestHandler } from "@sveltejs/kit";
+import markdownpdf from 'markdown-pdf'
+import tmp from 'tmp'
+import fs from 'fs'
 
 export const POST: RequestHandler = async ({ request }) => {
-    let markdown = await request.text()
+    let markdownText = await request.text()
 
-    let response: Response = new Response(markdown, { status: 200 })
+    const tmpMdFile = tmp.fileSync();
+    fs.writeFileSync(tmpMdFile.name, markdownText)
+
+    const tmpPdfFile = tmp.fileSync();
+
+    let pdfFile = new Promise((resolve, reject) => markdownpdf().from(tmpMdFile.name).to(tmpPdfFile.name, () => {
+        fs.readFile(tmpPdfFile.name, (err, data) => err ? reject(err) : resolve(data))
+    }))
+
+    let response: Response = new Response(await pdfFile, { status: 200 })
 
     return response
 };
